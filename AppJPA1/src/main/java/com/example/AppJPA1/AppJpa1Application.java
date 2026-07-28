d0e26280623ab45d82ee55e8dceed5ce716033b5
Deekonda_venu <venudeekonda786@gmail.com>
2026-07-28 23:40:28 +0530

    learning springboot

---
+++ AppJPA1/src/main/java/com/example/AppJPA1/AppJpa1Application.java
@@ -0,0 +1,13 @@
+package com.example.AppJPA1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppJpa1Application.class, args);
+	}
+
+}
---
+++ AppJPA1/src/test/java/com/example/AppJPA1/AppJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AppJPA1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppJpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
