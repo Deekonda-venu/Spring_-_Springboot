e7641ed38d408069df76aabc5ed18b09906209fd
Deekonda_venu <venudeekonda786@gmail.com>
2026-07-29 00:08:49 +0530

    learning springboot

---
+++ AppJPA1/src/main/java/com/example/AppJPA1/model/Student.java
@@ -0,0 +1,22 @@
+package com.example.AppJPA1.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="Student")
+@Data
+
+public class Student {
+
+    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int sid;
+    String susername;
+    String sfname;
+    String slname;
+    long sphone;
+    String semail;
+
+
+}
