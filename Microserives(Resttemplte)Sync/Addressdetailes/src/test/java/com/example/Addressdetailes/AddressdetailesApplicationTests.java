c742311cfb2f39b0f8bb2adf8c99899bc51d0558
Deekonda_venu <venudeekonda786@gmail.com>
2026-08-09 19:23:34 +0530

    learning springboot

---
+++ Microserives(Resttemplte)Sync/Addressdetailes/src/test/java/com/example/Addressdetailes/AddressdetailesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Addressdetailes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressdetailesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
